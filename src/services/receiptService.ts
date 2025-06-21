c96ed3f30a84cc66c2bd609cdfdd1d7cb3122c8f
조혜린 <hyer1205@gmail.com>
2025-06-21 17:55:41 +0900

    ♻️ Refactor: 영수증의 format을 맞출 때, 사용하는 text 길이 함수를 textUtils.ts로 분리

--- src/services/receiptService.ts
+++ src/services/receiptService.ts
@@ -1,18 +1,5 @@
 import type { Receipt } from '@/types/receipt';
-
-const getDisplayLength = (str: string) => {
-  let length = 0;
-  for (let i = 0; i < str.length; i += 1) {
-    const char = str.charAt(i);
-    // 한글은 2자리, 영문/숫자는 1자리
-    if (char.match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/)) {
-      length += 2;
-    } else {
-      length += 1;
-    }
-  }
-  return length;
-};
+import { getDisplayLength } from '@/utils/textUtils';
 
 export const createReceipt = (receipt: Receipt) => {
   const WIDTH = 25;
